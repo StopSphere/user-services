3ec2e12749cc90d6c23e983d6ea8e771ca389240
Monudhakad1 <monudhakad055@gmail.com>
2026-04-20 11:37:07 +0530

    User service setup : Done

---
+++ src/test/java/com/stopsphere/userservice/user_services/UserServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stopsphere.userservice.user_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
